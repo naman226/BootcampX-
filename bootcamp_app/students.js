370d094eb1d1c692af3621b371870bf1569fbad5
Naman Dhalla <namandhalla2261994@gmail.com>
2022-06-30 04:07:14 +0000

    Add SQL injection protection

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -6,16 +6,18 @@ const pool = new Pool({
   host: 'localhost',
   database: 'bootcampx'
 });
-
 pool.connect();
+
+const limit = process.argv[3] || 5;
+const cohortName = process.argv[2];
 
 pool.query(`
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
+WHERE cohorts.name LIKE $1        
+LIMIT $2;
+`, [`%${cohortName}%`, limit])
   .then(res => {
     res.rows.forEach(user => {
       console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -9,6 +9,8 @@ const pool = new Pool({
 
 pool.connect();
 
+const cohortName = process.argv[2] || 'JUL02';
+
 pool.query(`SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM assistance_requests
 JOIN teachers
@@ -17,11 +19,10 @@ JOIN students
 ON students.id = student_id
 JOIN cohorts
 ON cohorts.id = cohort_id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name = $1 
 ORDER BY teacher;
-`)
+`, [cohortName])
   .then(res => {
-    console.log(res.rows);
     res.rows.forEach(user => {
       console.log(`${user.cohort}: ${user.teacher}`);
     });
